a1f0cfa5ac21205f38c251e8674492e2411da755
lucas <lucasbezerra.rocinha@gmail.com>
2023-03-18 15:24:06 -0300

    alteração no banner

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -1,16 +1,21 @@
-
+import { useState } from 'react';
 import './style.css'
 
 const Banner = (props) => {
 
-    return (
+    const [nome, setNome] = useState('Desenvolvedor Front End - React Js');
 
+    function loading() {
+        
+    }
+
+    return (
 
         <section className="containerBanner">
             <div className='containerHello'>
                 <p className={props.background ? 'firstParagraphDark' : 'firstParagraphLight'}>Hello World!</p>
                 <h1 className={props.background ? 'titleDark' : 'titleLight'}>Oi, sou Lucas Agapito...</h1>
-                <h2 className={props.background ? 'subTitleDark' : 'subTitleLight'} data-text="DesenvolvedorFrontEnd" id='textMessage'>_____________________</h2>
+                <h2 className={props.background ? 'subTitleDark' : 'subTitleLight'} id='textMessage'>{nome}</h2>
             </div>
         </section>
     )
