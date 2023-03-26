6ce222e1747b460a46d67784e8670c8112e4db91
lucas <lucasbezerra.rocinha@gmail.com>
2023-03-26 16:59:04 -0300

    Atualização no subtitulo

--- src/Componentes/Banner/index.js
+++ src/Componentes/Banner/index.js
@@ -1,24 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css'
 
-const Banner = (props) => {
+export default function Banner(props) {
+    const [nome, setNome] = useState('');
+    const [index, setIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [typingSpeed, setTypingSpeed] = useState(150);
 
-    const [nome, setNome] = useState('Desenvolvedor Front End - React Js');
+    const [subject, setSubject] = useState(true);
 
-    function loading() {
-        
-    }
+    useEffect(() => {
+        const texto = 'Desenvolvedor Front End - React Js';
+
+        if (isDeleting) {
+            setNome(texto.substring(0, index - 1));
+        } else {
+            setNome(texto.substring(0, index + 1));
+        }
+
+        if (!isDeleting && index === texto.length) {
+            setIsDeleting(true);
+            setTypingSpeed(100)
+        } else if (isDeleting && index === 0) {
+            setIsDeleting(false);
+            setTypingSpeed(150);
+        }
+
+        const delay = isDeleting ? typingSpeed / 2 : typingSpeed;
+
+        const timer = setTimeout(() => {
+            setIndex(isDeleting ? index - 1 : index + 1);
+        }, delay);
+
+        return () => clearTimeout(timer);
+    }, [index, isDeleting]);
+
+    useEffect(() => {
+        const timerSubject = setInterval(() => {
+            setSubject(s => !s);
+        }, 3000);
+
+        return () => clearInterval(timerSubject);
+    }, []);
+
 
     return (
-
         <section className="containerBanner">
             <div className='containerHello'>
-                <p className={props.background ? 'firstParagraphDark' : 'firstParagraphLight'}>Hello World!</p>
+                <p className={`hello ${subject ? 'show-en' : 'show-pt'} ${props.background ? 'firstParagraphDark' : 'firstParagraphLight'}`}>{subject ? 'Hello World!' : 'Olá Mundo!'}</p>
                 <h1 className={props.background ? 'titleDark' : 'titleLight'}>Oi, sou Lucas Agapito...</h1>
                 <h2 className={props.background ? 'subTitleDark' : 'subTitleLight'} id='textMessage'>{nome}</h2>
             </div>
         </section>
     )
 }
-
-export default Banner;
